c6dd19ad32479fcb0b5cc4bb285bf90755c96259
GenaPlem <shainkill1337@gmail.com>
2025-03-04 14:34:24 +0000

    Add function to get recipes from json-server and display it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,51 @@
-export default function Home() {
-  return <main>home</main>;
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface Recipe {
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+  id: string;
+}
+
+async function getRecipes(): Promise<Recipe[]> {
+  const result = await fetch("http://localhost:4000/recipes");
+
+  return result.json();
+}
+
+export default async function Home() {
+  const recipes = await getRecipes();
+
+  return (
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id}>
+            <CardHeader>
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter>
+              <button>View Recipe</button>
+              {recipe.vegan && <p>Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
 }
